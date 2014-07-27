6e14f8f7e96c7e92bf9dc2d2675ff068e2889a67
Takahiro Masaki <tak.msk2580@gmail.com>
2014-07-27 22:00:13 +0900

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,14 @@ ruby '2.1.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
+
+gem 'bootstrap-sass'
+gem 'sprockets'
+
+group :assets do
+    gem 'compass-rails'
+end
+
 # Use sqlite3 as the database for Active Record
 group :development do
     gem 'sqlite3'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,12 @@ require 'capybara'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__) 
+require 'rspec/rails'
+
 RSpec.configure do |config|
+    config.include Rails.application.routes.url_helpers
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
